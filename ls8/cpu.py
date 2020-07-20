00881dceea25f1a067b24ac61fd4314c17cf2e31
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-07-20 15:17:49 -0600

    Day 1 done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,11 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.RAM = [0] * 256         # Memory list to hold up to 256 commands
+        self.register = [0] * 8      # Register to store up to 8 values
+        self.PC = 0                  # Program counter
+        #self.register[7] = '0xF4'
+
 
     def load(self):
         """Load a program into memory."""
@@ -27,7 +31,8 @@ class CPU:
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.RAM[address] = instruction
+            #print(instruction)
             address += 1
 
 
@@ -35,10 +40,11 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
 
     def trace(self):
         """
@@ -47,19 +53,60 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.PC,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
+
     def run(self):
         """Run the CPU."""
-        pass
+        # Storing command codes for easier reference
+        HLT = 0b00000001        # Halt execution
+        LDI = 0b10000010        # Load immediate
+        PRN = 0b01000111        # Print
+
+        # While loop to keep running the program
+        run_again = True
+        while run_again:
+
+            #self.trace()
+
+            IR = self.RAM[self.PC]         # Load the instruction from RAM at PC index
+            
+            operand_a = self.RAM[self.PC+1]     # Load the next two locations of RAM in case
+            operand_b = self.RAM[self.PC+2]     # the instruction at RAM[PC] needs them
+
+            if IR == HLT:
+                # Break the loop
+                run_again = False
+            
+            elif IR == LDI:
+                # At the next address in the memory, input the value
+                # that follows
+                self.register[operand_a] = operand_b
+                self.PC += 3
+            
+            elif IR == PRN:
+                # Print the value at the following address
+                print(self.register[operand_a])
+                self.PC += 2
+        
+
+
+    def ram_read(self, MAR):
+        """Read the contents of RAM at location MAR."""
+        return self.RAM[MAR]
+
+
+    def ram_write(self, MAR, MDR):
+        """Write MDR to the location MAR in RAM."""
+        self.RAM[MAR] = MDR
